154b732ed1cccbd17d82e0ddeb4263b9ccbf741f
Fabian Szyman <fabianszyman@gmail.com>
2022-05-10 13:57:42 +0200

    add fallback cases, when no search results appear on screen

--- README.md
+++ README.md
@@ -5,9 +5,13 @@ App that is running Node.js to automate instagram actions (e.g. liking someones 
 ## Open To Dos:
 - Add Follow function 
 - Disable CTAs when action is started
-- Add Stop CTA 
+- Add Stop CTA (feetch information vomm DB and add check to actions, if maybe the user stopped the actions)
 - Add Session ID into DB 
 - Check for latest Session 
 - Give user action feedback inside the FE while script is running
 - Add method to remove hashtag sympols from searchterm input 
 - add method to remove empty spaces between searchterms
+- Handle case when User has wrong password 
+- Handle case when User tried to log in to many times
+- add error logging DB
+- Change nedb to MongoDB cloud 
--- index.js
+++ index.js
@@ -7,10 +7,36 @@ app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 var Datastore = require('nedb');
 
+
+// URL collection 
+const loginURL = 'https://www.instagram.com/';
+const tagsURL = 'https://www.instagram.com/explore/tags/';
+
+// CSS variables for selection of elements
 var imageURLdivElementOfNewestPost = '#react-root > section > main > article > div:nth-child(3) > div > div:nth-child(1) > div:nth-child(1) > a';
 var userInformationDivElement = 'div.e1e1d > div > span > a';
 var emptyHeartIcon = 'section >span > button > div >span > svg[fill="#262626"]';
 var loginErrorMessageTooManyLogins = 'p[data-testid="login-error-message"]';
+var searchTermSuggestionFirstResult = 'div:nth-child(1)>a[class="-qQT3"]';
+var clearSearchTermInputIcon = '.coreSpriteSearchClear';
+var pageNotFountTitle = '#react-root > section > main > div > div > h2';
+
+// amount Action Variables 
+const minMiliSeconds = 5000;
+const maxMiliSeconds = 10000;
+
+var errorLimitReached = false;
+var maxAmountOfAction = 10;
+var maxAmountOfActionMinusOne = maxAmountOfAction - 1;
+var amountOfActionsDone = 0;
+
+var searchResultListShown = false; 
+var noSearchSuggestionAmountOfTrys = 0;
+var amountOfSearchSuggestionTrys = 3;
+
+var amountOfErrosLogged = 0;
+var errorLogLimit = 5;
+
 
 var dbActions = new Datastore({ filename: 'dbActions.db' , autoload: true });
 var dbUsers = new Datastore({ filename: 'dbUsers.db' , autoload: true });
@@ -23,9 +49,9 @@ dbUsers.loadDatabase(function(err) {
     // Start issuing commands after callback...
 });
 
-dbUsers.findOne({ username: 'sneaker_mania_berlin' }, function(err, doc) {
-    console.log('Found User:', doc.username);
-});
+//dbUsers.findOne({ username: 'sneaker_mania_berlin' }, function(err, doc) {
+//    console.log('Found User:', doc.username);
+//});
 
 //dbActions.findOne({ username: 'sneaker_mania_berlin' }, function(err, doc) {
 //    console.log('Found Action:', doc.username);
@@ -54,7 +80,7 @@ app.post('/api', (request, response) => {
             headless: false // setting this to true will not run the UI
         }); 
         const page = await browser.newPage();
-        await page.goto('https://www.instagram.com/');
+        await page.goto(loginURL);
         await page.waitForTimeout(2000);
         await page.click('body > div.RnEpo.Yx5HN._4Yzd2 > div > div > button.aOOlW.bIiDR');
         await page.waitForTimeout(2000);
@@ -70,83 +96,152 @@ app.post('/api', (request, response) => {
             var feedback = "failed";
         } else {
             var feedback = "success";
-        }
-
-        var maxAmountOfAction = 15;
-        var maxAmountOfActionMinusOne = maxAmountOfAction - 1;
-        var amountOfActionsDone = 0;
-        for(i=1; amountOfActionsDone<=maxAmountOfActionMinusOne; i++){
-
-            if (i != 1) {
-                // wait random amount of seconds 
-                const minMiliSeconds = 5000;
-                const maxMiliSeconds = 10000;
-                await page.waitForTimeout(randomNumber(minMiliSeconds, maxMiliSeconds));
-            }
-
-            const searchTerm = getRandomSearchTerm();
-            await page.waitForTimeout(4000);
-            await page.type('input[type="text"]', searchTerm);
-            await page.click('div:nth-child(1)>a[class="-qQT3"]'); 
-            // get number of total amount of posts for a certain serachTerm
-            await page.waitForSelector(imageURLdivElementOfNewestPost);
-            var amountOfTotalPostsForCertainTermCssSelector = 'span[class="g47SY "]';
-            if (await page.$(amountOfTotalPostsForCertainTermCssSelector) !== null){
-                const amountOfTotalPostsForCertainTerm = await page.$eval(amountOfTotalPostsForCertainTermCssSelector, span => span.textContent);
-                console.log('amount of total posts is '+ amountOfTotalPostsForCertainTerm + ' for search term '+ searchTerm);
-            } else {
-                console.log('no total number appeared on screen');
-            }
-            // query for newest Image URL's appearing --- Skip the first 9 top results 
-            var productURLOfNewestPost = await page.$eval(imageURLdivElementOfNewestPost, div => div.href);
-            console.log('productURLOfNewestPost: '+ productURLOfNewestPost);
-            // go to post URL and like post if it not already liked
-            await page.goto(productURLOfNewestPost);
+            // loop over amount of actions
+            for (i=1;amountOfActionsDone<=maxAmountOfActionMinusOne;i++){
             
-            // check if productURL still exists
-            if (await page.$('h2._7UhW9') !== null){
-                console.log('post probably does not exists anymore');
-            } else {
-                var usernameOfInstagramProfile = await page.$eval(userInformationDivElement, div => div.textContent); // get username
-                console.log('usernameOfInstagramProfile: '+ usernameOfInstagramProfile);
-                var userProfileURL = await page.$eval(userInformationDivElement, div => div.href); // get user profile URL
-                console.log('userProfileURL: '+ userProfileURL);
-                // click like Button 
-                actionType = 'likeByTags';
-                if (await page.$(emptyHeartIcon) !== null){
-                    await page.click(emptyHeartIcon);
-                    console.log("Post liked");
-                    amountOfActionsDone++;
-                    var timestampLastLikeAction = Date.now();
-                    dbObjectActions = {
-                        created_at: timestampLastLikeAction,
-                        username: request.body.username,
-                        searchTerms: request.body.arrayOfSearchTerms,
-                        seachTermUsed: searchTerm,
-                        usernameOfInstagramProfile: usernameOfInstagramProfile,
-                        action: {
-                            timestampLastLikeAction: timestampLastLikeAction,
-                            actionType: actionType,
-                            url: productURLOfNewestPost,
-                            userProfileURL: userProfileURL,
-                            amountOfActionsDone: amountOfActionsDone
+                // check if not too many erros were logged
+                if (amountOfErrosLogged>=errorLogLimit){
+                    console.log('Amount of Erros that were logged: '+ amountOfErrosLogged);
+                    console.log('Browser closed due to too many errors that were logged');
+                    var feedback = 'too many errors logged'
+                    var timestampActionsEnded = Date.now();
+                    var errorLimitReached = true;
+                    await browser.close();
+                    await response.json({
+                        feedback,
+                        created_at: timestampActionsStarted, 
+                        actions_ended: timestampActionsEnded,
+                        dbObjectUsers,
+                        amountOfActionsDone,
+                        status: "failed"
+                    });
+                } else {
+                
+                    // wait random amount of seconds 
+                    if (i != 1) {
+                        await page.waitForTimeout(randomNumber(minMiliSeconds, maxMiliSeconds));
+                    }
+                
+                    // fetch random searchTerm from SearchTerms array
+                    var searchTerm = getRandomSearchTerm();
+                    var searchTermWithoutHashtag = searchTerm.substring(1);
+                    await page.waitForTimeout(2000);
+                
+                    // try to open tag result list through typing in the searchTerm inside Search input
+                    if (!errorLimitReached && !searchResultListShown){
+                        for(k=0; noSearchSuggestionAmountOfTrys<amountOfSearchSuggestionTrys; k++){
+                            console.log('Trying to open searchTerms with Typing inside search input field');
+                            await page.type('input[type="text"]', searchTerm);
+                            await page.waitForTimeout(4000);
+                        
+                            // check if searchTerm suggestion dropdown list is showing up
+                            if (await page.$(searchTermSuggestionFirstResult) !== null){
+                                console.log('CHECKPOINT 1');
+                                await page.click(searchTermSuggestionFirstResult);
+                            
+                                // check if page not found error appears
+                                if (await page.$(pageNotFountTitle) !== null){
+                                    console.log('CHECKPOINT 2');
+                                    await page.waitForTimeout(randomNumber(minMiliSeconds, maxMiliSeconds));
+                                    console.log('page not found error no. :'+ noSearchSuggestionAmountOfTrys);
+                                    noSearchSuggestionAmountOfTrys++;
+                                    amountOfErrosLogged++;
+                                } else {
+                                    console.log('CHECKPOINT 3');
+                                    var searchResultListShown = true;
+                                }
+                            }
+                            else {
+                                console.log('CHECKPOINT 4');
+                                var searchTerm = getRandomSearchTerm();
+                                await page.click(clearSearchTermInputIcon);
+                                noSearchSuggestionAmountOfTrys++;
+                                amountOfErrosLogged++;
+                                console.log('No SearchTerm Suggestion showed up');
+                            }
                         }
                     }
+                
+                    // try to open tag result list immediately
+                    if (!errorLimitReached && !searchResultListShown){
+                        await page.goto(tagsURL+searchTermWithoutHashtag);
+                        if (await page.$(imageURLdivElementOfNewestPost)!== null){
+                            var searchResultListShown = true;
+                        } else if (await page.$(pageNotFountTitle) !== null) {
+                            console.log('No Search Results found');
+                            amountOfErrosLogged++;
+                        }
+                    }
+                
+                    if (searchResultListShown){
+                    
+                        var searchResultListShown = false;
+                        // get number of total amount of posts for a certain serachTerm
+                        await page.waitForSelector(imageURLdivElementOfNewestPost);
+                        var amountOfTotalPostsForCertainTermCssSelector = 'span[class="g47SY "]';
+                        if (await page.$(amountOfTotalPostsForCertainTermCssSelector) !== null){
+                            const amountOfTotalPostsForCertainTerm = await page.$eval(amountOfTotalPostsForCertainTermCssSelector, span => span.textContent);
+                            console.log('amount of total posts is '+ amountOfTotalPostsForCertainTerm + ' for search term '+ searchTerm);
+                        } else {
+                            console.log('no total number appeared on screen');
+                        }
+                    
+                        // query for newest Image URL's appearing --- Skip the first 9 top results 
+                        var productURLOfNewestPost = await page.$eval(imageURLdivElementOfNewestPost, div => div.href);
+                        console.log('productURLOfNewestPost: '+ productURLOfNewestPost);
+                    
+                        // go to post URL and like post if it not already liked
+                        await page.goto(productURLOfNewestPost);
+                    
+                        // check if productURL still exists
+                        if (await page.$('h2._7UhW9') !== null){
+                            console.log('post probably does not exists anymore');
+                        } else {
+                            var usernameOfInstagramProfile = await page.$eval(userInformationDivElement, div => div.textContent); // get username
+                            console.log('usernameOfInstagramProfile: '+ usernameOfInstagramProfile);
+                            var userProfileURL = await page.$eval(userInformationDivElement, div => div.href); // get user profile URL
+                            console.log('userProfileURL: '+ userProfileURL);
                         
-                    dbActions.insert(dbObjectActions);
-                    console.log('Amount of Actions: '+ amountOfActionsDone);
-                } else {
-                    console.log("Post was already liked");
+                            // click like Button 
+                            actionType = 'likeByTags';
+                            if (await page.$(emptyHeartIcon) !== null){
+                                await page.click(emptyHeartIcon);
+                                console.log("Post liked");
+                                amountOfActionsDone++;
+                                var timestampLastLikeAction = Date.now();
+                                dbObjectActions = {
+                                    created_at: timestampLastLikeAction,
+                                    username: request.body.username,
+                                    searchTerms: request.body.arrayOfSearchTerms,
+                                    seachTermUsed: searchTerm,
+                                    usernameOfInstagramProfile: usernameOfInstagramProfile,
+                                    action: {
+                                        timestampLastLikeAction: timestampLastLikeAction,
+                                        actionType: actionType,
+                                        url: productURLOfNewestPost,
+                                        userProfileURL: userProfileURL,
+                                        amountOfActionsDone: amountOfActionsDone
+                                    }
+                                }
+                                dbActions.insert(dbObjectActions);
+                                console.log('Amount of Actions: '+ amountOfActionsDone);
+                            } else {
+                                console.log("Post was already liked");
+                            }
+                        }
+                    }
                 }
             }
         }
+
         // get random element from Array -- START 
         function getRandomSearchTerm() {
-            var randomSerchTermWithoutHashtag = arrayOfSearchTerms[Math.floor(Math.random()*arrayOfSearchTerms.length)];
-            var randomSerchTerm = '#'+randomSerchTermWithoutHashtag;
-            return randomSerchTerm;
+            var randomSearchTermWithoutHashtag = arrayOfSearchTerms[Math.floor(Math.random()*arrayOfSearchTerms.length)];
+            var randomSearchTerm = '#'+randomSearchTermWithoutHashtag;
+            return randomSearchTerm;
         }
         // get random element from Array -- END
+
         // function to wait for random amount of seconds --START
         function randomNumber(min, max){
             min = Math.ceil(min);
@@ -167,7 +262,6 @@ app.post('/api', (request, response) => {
             amountOfActionsDone,
             status: "success"
         });
-        await browser.close();
     }
     main();
 });
